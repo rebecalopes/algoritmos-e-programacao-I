7233a47ef340e42e52b6c7ef2accb97a5469c6dd
rebecalopes <rebeca.lopes.141@gmail.com>
2020-08-22 20:06:19 -0300

    exercicio 2.4 de operacoes com strings

---
+++ problemasPraticos2.4.py
@@ -0,0 +1,26 @@
+#Comece executando as instruçoes de atribuiçao
+
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+
+#Escreva expressões Python usando s1, s2 e s3 e os operadores + e * a fim de avaliar para
+#'ant bat cod'
+expre1 = s1 + ' ' + s2 + ' ' + s3
+print (expre1)
+
+#'ant ant ant ant ant ant ant ant ant ant'
+expre2 = (s1 + ' ') * 10
+print(expre2)
+
+#'ant bat bat cod cod cod'
+expre3 = s1 + ' ' + (s2 + ' ' * 2) + (s3 + ' ' * 3)
+print(expre3)
+
+#'ant bat ant bat ant bat ant bat ant bat ant bat ant bat'
+expre4 = s1 + ' ' + s2 + ' ' + s1 + ' ' + s2 + ' ' + s1 + ' ' + s2 + ' ' + s1 + ' ' + s2 + ' ' + s1 + ' ' + s2 + ' ' + s1 + ' ' + s2 + ' ' + s1 + ' ' + s2
+print(expre4)
+
+#'batbatcod batbatcod batbatcod batbatcod batbatcod'
+expre5 = (s1+s2+s3+' ') * 5
+print(expre5)
