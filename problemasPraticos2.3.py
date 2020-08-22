f280570ab8b8cc951a3ca3a7e7ff26ed012faa58
rebecalopes <rebeca.lopes.141@gmail.com>
2020-08-22 19:50:02 -0300

    exercicio 2.3 de variaveis e atribuiçoes

---
+++ problemasPraticos2.3.py
@@ -0,0 +1,11 @@
+#Atribua o valor inteiro 3 a variavel a.
+a = 3
+print(a)
+
+#Atribua 4 a variavel b.
+b = 4 
+print(b)
+
+#Atribua a variavel c o valor da expressão a * a + b * b.
+c = a * a + b * b
+print (c)
