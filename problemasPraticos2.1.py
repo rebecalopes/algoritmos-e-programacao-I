cc3407e675ffc47dad92062520313f2145946461
rebecalopes <rebeca.lopes.141@gmail.com>
2020-08-22 19:14:01 -0300

    exercicios 2.1 de expressões algébricas

---
+++ problemasPraticos2.1.py
@@ -0,0 +1,28 @@
+#2.1
+#A soma dos 5 primeiros inteiros positivos.
+soma = 1 + 2 + 3 + 4 + 5
+print (soma)
+
+# A idade media de Sara (idade 23) Mark (idade 19) e Fatima (idade 31)
+idadeMedia = (23 + 19 + 31) / 3
+print (f'{idadeMedia:.1f}')
+
+#O numero de vezes que 73 cabe em 403.
+quantidade = 403 // 73
+print (quantidade)
+
+#O resto de quando 403 e dividido por 73.
+resto = 403 % 73
+print (resto)
+
+#2 a 10a potência.
+potencia = 2 ** 10
+print (potencia)
+
+#O valor absoluto da distancia entre a altura de Sara (54 polegadas) e a altura de Mark (57 polegadas).
+distancia = abs(54 - 57)
+print(distancia)
+
+#O menor preço entre os seguintes preços: R$ 34,99, R$ 29,95 e R$ 31,50.
+menor = min(34.99, 29.95, 31.50)
+print (menor)
