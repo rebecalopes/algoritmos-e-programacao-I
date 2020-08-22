24366ea1141ec45be6a677012b0e395e8e58a637
rebecalopes <rebeca.lopes.141@gmail.com>
2020-08-22 19:38:16 -0300

    exercicios 2.2 de expressões Booleanas

---
+++ problemasPraticos2.2.py
@@ -0,0 +1,27 @@
+#A soma de 2 e 2 e menor que 4.
+soma = (2 + 2) < 4
+print(soma) 
+
+#O valor de 7 // 3 e igual a 1 + 1.
+valor = 7 // 3 == (1+1)
+print(valor)
+
+#A soma de 3 ao quadrado e 4 ao quadrado e igual a 25.
+quadrado = (3 ** 2 + 4 ** 2) == 25
+print(quadrado)
+
+#A soma de 2, 4 e 6 e maior que 12.
+maior = (2 + 4 + 6) > 12
+print(maior)
+
+#1387 e divisivel por 19.
+divisao = 1387%19 == 0
+print (divisao)
+
+#31 é par. (Dica: o que o resto lhe diz quando você divide por 2?)
+par = 31%2 == 0
+print(par)
+
+#O preço mais baixo dentre R$ 34,99, R$ 29,95 e R$ 31,50 é menor que R$ 30,00.
+preco = min(34.99, 29.95, 31.50) < 30.00
+print(preco)
