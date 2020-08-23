07e4ffe9330c0a8c69f15b0988c92e8d756ca339
rebecalopes <rebeca.lopes.141@gmail.com>
2020-08-23 13:49:04 -0300

    exercicio 2.7 sobre listas

---
+++ problemasPraticos/problemasPraticos2.7.py
@@ -0,0 +1,20 @@
+notas = [9 ,7 ,7 ,10 ,3 ,9 ,6 ,6 ,2]
+
+#expressao que avalia para o numero de 7 notas.
+notas.count(7)
+
+#instruçao que muda a ultima nota para 4.
+notas.append(4)
+
+#expressao que avalia para a nota mais alta.
+notas.sort()
+notas[-1]
+
+#instruçao que classifica as notas da lista.
+notas.sort()
+
+#expressao que avalia para a media das notas.
+soma = sum(notas)
+quantidade = len(notas)
+media = soma / quantidade
+print(soma)
