69697390a76f26627597005a79afa15fc354f296
rebecalopes <rebeca.lopes.141@gmail.com>
2020-08-23 15:05:56 -0300

    simplificando a solução

--- problemasPraticos/problemasPraticos2.6.py
+++ problemasPraticos/problemasPraticos2.6.py
@@ -2,5 +2,5 @@ palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
 
 #duas expressoes como a primeiro e a ultima palavras em palavras, na ordem do dicionario.
 
-palavras[0]
-palavras[-1]
+min(palavras)
+max(palavras)
