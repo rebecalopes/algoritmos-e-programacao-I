a810ef3645feea6d2288f19118d71999e97c8f66
rebecalopes <rebeca.lopes.141@gmail.com>
2020-08-26 01:58:53 -0300

    recuperando repositorio pos queda, e adicionando exercicios de calcilo de triangulo com e sem interaçao

---
+++ exercicios propostos/exercicio5.py
@@ -0,0 +1,4 @@
+base = 9
+altura = 6
+area = (base * altura) / 2
+print('A area do triangulo =', area)
---
+++ exercicios propostos/exercicio6.py
@@ -0,0 +1,6 @@
+base = int(input('Indique a base '))
+altura = int(input('Indique a altura '))
+
+area = (base * altura) / 2
+
+print('O valor da area e', area)
