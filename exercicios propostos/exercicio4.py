4205928587b45332951326a852d0b7441250ca94
rebecalopes <rebeca.lopes.141@gmail.com>
2020-08-26 01:03:08 -0300

    praticando aritmetica e comando de saida de dados

---
+++ exercicios propostos/exercicio4.py
@@ -0,0 +1,9 @@
+print('Inicio do programa')
+print(' ')
+fixo = 14.00
+vendas = 5.00
+comissao = 6 / 100
+faturamento = fixo + vendas * comissao
+print('Faturamento do mes = {0:.2f}'.format(faturamento))
+print(' ')
+print('Fim do programa')
