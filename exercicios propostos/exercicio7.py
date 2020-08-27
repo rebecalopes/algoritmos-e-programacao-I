57938510aef84efa9925414a4fb939dbbae405a8
rebecalopes <rebeca.lopes.141@gmail.com>
2020-08-26 23:20:35 -0300

    calculo salario bruto a partir de hrs tabalhadas

---
+++ exercicios propostos/exercicio7.py
@@ -0,0 +1,10 @@
+horasTrabalhadas = 163
+valorHora = 14.25
+horasExtra = 20
+
+totalHoras = (horasTrabalhadas * valorHora)
+totalExtra = (horasExtra * valorHora) * 2
+
+valorTotal = totalExtra + totalHoras
+
+print(valorTotal)
