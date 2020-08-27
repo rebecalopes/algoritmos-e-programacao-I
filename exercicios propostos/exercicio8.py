3fab2d5ac4d75104e7032e47f5a6d98fc0b6d9bd
rebecalopes <rebeca.lopes.141@gmail.com>
2020-08-26 23:47:40 -0300

    expressoes aritmeticas

---
+++ exercicios propostos/exercicio8.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+a = 4
+b = 5
+c = 1
+x1 = 1
+x2 = 4
+y1 = 1
+y2 = 5
+
+r = (a + b) / 2
+print(r)
+
+x = (-b + sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+print(x)
+
+r = (3 * a + 2 * b) / (a + b)
+print(r)
+
+z = 7.6 * a - b ** 1.7
+print(z)
+
+d = sqrt((x1 - x2) ** 2  + (y1 - y2) ** 2)
+print(d)
