1e7190bc830b6620b01c47a65537c3740286810c
rebecalopes <rebeca.lopes.141@gmail.com>
2020-09-01 22:50:07 -0300

    traduzindo instruçoes condicionais em if

--- problemas praticos/problemasPraticos3.2.py
+++ problemas praticos/problemasPraticos3.2.py
@@ -2,3 +2,16 @@ idade = eval(input('Digite sua idade '))
 
 if idade > 62:
     print('Voce pode obter beneficios de pensao')
+
+nome = str(input('Qual o seu nome? '))
+
+jogadores = ['Musial', 'Aaraon', 'Williams', 'Gehrig', 'Ruth']
+
+if nome in jogadores:
+    print('Um dos 5 maiores jogadores de beisebol de todos os tempos!')
+
+golpes = eval(input('Quantidade de golpes '))
+defesa = eval(input('Numero de defesa '))
+
+if golpes > 10 and defesa == 0:
+    print('Voce esta morto')
